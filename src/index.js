ccc0d6dce462bd98330d8fec7a27d5da318fe8dc
Manan V. Panchal <mananvpanchal@gmail.com>
2017-10-12 17:59:50 +0530

    only boilerplate

--- src/index.js
+++ src/index.js
@@ -1,29 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Table from './table';
 
-const renderCell = (eventListeners, rowIdx, colIdx, value, 
-  mouseClicked, mouseDoubleClicked, keyDown, mouseEntered, mouseOut,
-  evtRowIdx, evtColIdx) => {
-  const style = { border: "1px solid black" };
-  style.width='100px';
-  style.height='30px';
- /* if(keyDown && rowIdx === evtRowIdx && colIdx === evtColIdx) {
-    style.backgroundColor = 'yellow';
-  } else if(mouseDoubleClicked && rowIdx === evtRowIdx && colIdx === evtColIdx) {
-    style.backgroundColor = 'blue';
-  } else if(mouseClicked && rowIdx === evtRowIdx && colIdx === evtColIdx) {
-    style.backgroundColor = 'green';
-  } else if(mouseEntered && rowIdx === evtRowIdx && colIdx === evtColIdx) {
-    style.backgroundColor = 'red';
-  }*/
-  if(mouseOut && rowIdx === evtRowIdx && colIdx === evtColIdx) {
-    console.log('mouseOut');
-    style.backgroundColor = 'red';
-  }// else {
-  //  style.backgroundColor = 'white';
-  //}
-  return <td tabIndex={1} {...eventListeners} style={style} key={rowIdx+'-'+colIdx}>{value}</td>;
-};
-
-ReactDOM.render(<div><Table renderCell={renderCell} /></div>, document.getElementById('app'));
+ReactDOM.render(<div>Hello</div>, document.getElementById('app'));
